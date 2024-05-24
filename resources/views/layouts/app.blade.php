5f1db000cfcc3c8fa34a7b931cb40c6fdad5fe55
moesslan <moesslan@edu.htl-villach.at>
2024-05-24 11:42:03 +0200

    css_fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
     </head>
     <body class="bg-white dark:bg-gray-700 text-black dark:text-white">
         <div class="min-h-screen">
